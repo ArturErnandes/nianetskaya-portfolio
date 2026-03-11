fc16ae36d7a61b28ed37bf58315d2462e108cdba
ArturErnandes <al211205@yandex.ru>
2026-03-11 16:51:24 +0300

    Ручка получения работ

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from database import get_works_list_db
 from logger import get_logger
@@ -9,7 +9,9 @@ logger = get_logger(__name__)
 app = FastAPI()
 
 
-
-@app.get("/get_works_list")
-async def get_works_list(section_name):
-    return await get_works_list_db(section_name)
+@app.get("/get_works_list", tags=["Works"], summary="Получение списка работ указанной категории")
+async def get_works_list(section_name: str):
+    try:
+        return await get_works_list_db(section_name)
+    except RuntimeError:
+        raise HTTPException(status_code=500, detail="load_error")
