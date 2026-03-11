f071214d73f908fbc0bdb9943c71f4bd9bbee57c
ArturErnandes <al211205@yandex.ru>
2026-03-11 20:32:42 +0300

    Ручка получения работы

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-from .database import get_works_list_db
+from .database import get_works_list_db, get_work_db
 from .config import AppConfig
+from .exceptions import WorkLoadError, WorkNotFoundError
 from .logger import get_logger
 
 
@@ -32,5 +33,15 @@ app.add_middleware(
 async def get_works_list(section_name: str):
     try:
         return await get_works_list_db(section_name)
-    except RuntimeError:
+    except WorkLoadError:
+        raise HTTPException(status_code=500, detail="load_error")
+
+
+@app.get("/get_work", tags=["Works"], summary="Получение данных указанной работы")
+async def get_work(work_id: int):
+    try:
+        return await get_work_db(work_id)
+    except WorkNotFoundError:
+        raise HTTPException(status_code=404, detail="not_found")
+    except WorkLoadError:
         raise HTTPException(status_code=500, detail="load_error")
