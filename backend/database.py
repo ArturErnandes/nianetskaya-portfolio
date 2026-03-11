e54f2fdc2110b57a502dcfded83b0783a1f71ab7
ArturErnandes <al211205@yandex.ru>
2026-03-11 20:33:20 +0300

    Функция получения работы

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import text
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from .config import DbConfig
-from .classes import WorkSchema
+from .classes import ClosedWorkSchema, OpenedWorkSchema
+from .exceptions import WorkLoadError, WorkNotFoundError
 from.logger import get_logger
 
 
@@ -14,19 +15,19 @@ engine = create_async_engine(
 new_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
-async def get_works_list_db(section_name: str) -> list[WorkSchema]:
+async def get_works_list_db(section_name: str) -> list[ClosedWorkSchema]:
     query = text("SELECT work_id, title, caption, img_name FROM works WHERE section_name = :section_name")
 
     try:
         async with new_session() as session:
-            works = await session.execute(query, {"section_name": section_name})
-            works_rows = works.mappings().all()
+            result = await session.execute(query, {"section_name": section_name})
+            works_rows = result.mappings().all()
 
             return [
-                WorkSchema(
+                ClosedWorkSchema(
                     id=row["work_id"],
                     title=row["title"],
-                    text=row["caption"],
+                    caption=row["caption"],
                     img_name=row["img_name"],
                 )
                 for row in works_rows
@@ -34,4 +35,29 @@ async def get_works_list_db(section_name: str) -> list[WorkSchema]:
 
     except Exception as e:
         logger.exception(f"Ошибка при получении работ категории {section_name}: {str(e)}")
-        raise RuntimeError("load_error") from e
+        raise WorkLoadError from e
+
+
+async def get_work_db(work_id: int) -> OpenedWorkSchema:
+    query = text("SELECT section_name, title, description, img_name FROM works WHERE work_id = :work_id")
+
+    try:
+        async with new_session() as session:
+            result = await session.execute(query, {"work_id": work_id})
+            work_data = result.mappings().first()
+
+            if work_data is None:
+                raise WorkNotFoundError
+
+            return OpenedWorkSchema(
+                section_name=work_data["section_name"],
+                title=work_data["title"],
+                description=work_data["description"],
+                img_name=work_data["img_name"],
+            )
+
+    except WorkNotFoundError:
+        raise
+    except Exception as e:
+        logger.exception(f"Ошибка при получении работы | id: {work_id} | Ошибка: {str(e)}")
+        raise WorkLoadError from e
