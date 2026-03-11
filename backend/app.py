640b486e2bb4880fcdf0bc4efbb53733a35cd95f
ArturErnandes <al211205@yandex.ru>
2026-03-11 16:34:14 +0300

    ручка получения работ раздела

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from database import get_works_list_db
 from logger import get_logger
 
 
 logger = get_logger(__name__)
 
 app = FastAPI()
+
+
+
+@app.get("/get_works_list")
+async def get_works_list(section_name):
+    return await get_works_list_db(section_name)
