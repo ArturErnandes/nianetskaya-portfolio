77db3ac82481a490b80904a3b139796f17345d82
ArturErnandes <al211205@yandex.ru>
2026-03-14 19:47:20 +0300

    перевод на js

---
+++ frontend/js/sections-config.js
@@ -0,0 +1,23 @@
+const SECTIONS_CONFIG = {
+    book_illustration: {
+        heading: "Книжная иллюстрация",
+    },
+    easel_graphics: {
+        heading: "Станковая графика",
+    },
+    graphic_design: {
+        heading: "Графический дизайн",
+    },
+    portfolio_presentation_design: {
+        heading: "Дизайн портфолио / презентаций",
+    },
+    art_objects: {
+        heading: "Арт объекты",
+    },
+    painting: {
+        heading: "Живопись",
+    },
+    textile_design: {
+        heading: "Дизайн текстиля",
+    },
+};
