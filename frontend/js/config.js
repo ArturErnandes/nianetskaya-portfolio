63474e2fc517520837d43b25cd886d06bbf305b8
ArturErnandes <al211205@yandex.ru>
2026-03-14 09:45:42 +0300

    Адрес бэкенда

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,5 @@
-const API_BASE_URL = "http://31.128.48.187:8080";
-const ASSETS_PATH = "../../assets";
+const ASSETS_PATH = "/assets";
 
 const API_ENDPOINTS = {
-    works: `${API_BASE_URL}/api/works`,
+    works: "/api/works",
 };
