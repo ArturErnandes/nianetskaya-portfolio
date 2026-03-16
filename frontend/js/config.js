9e2254dfc0fd000325b2a84c922aa1f779ddf7f3
ArturErnandes <al211205@yandex.ru>
2026-03-16 17:40:05 +0300

    добавлены ручки

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,6 @@
 const WORKS_ASSETS_PATH = "/assets/works";
 
 const API_ENDPOINTS = {
-    works: `$/api/works`,
-    randomWorks: `$/api/random_works`,
+    works: `/api/works`,
+    randomWorks: `/api/random_works`,
 };
