a75872ae61c3e3b1095a87a8349ea4dfc442d1c9
ArturErnandes <al211205@yandex.ru>
2026-04-18 23:54:28 +0300

    Рефакторинг api

---
+++ backend/api/router_constants.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+FRONTEND_HTML_DIR = PROJECT_ROOT / "frontend" / "html"
+WORKS_ASSETS_DIR = PROJECT_ROOT / "assets" / "works"
